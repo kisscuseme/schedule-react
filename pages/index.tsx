81ffb00d68f915358092ab2c29b486e698c735f8
2130056 <a2130056@2130056ui-MacBookPro.local>
2023-05-12 15:38:38 +0900

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react"
 import SignIn from "./signin";
 import Schedule from "./schedule";
+import { UserType } from "@/services/firebase/user.type";
 
 export default function Home() {
-  const [user, setUser] = useState(null as UserProps);
+  const [user, setUser] = useState(null as UserType);
   const router = useRouter();
   useEffect(() => {
     // if(!user){
